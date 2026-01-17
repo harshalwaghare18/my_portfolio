aff258776c96c3c0e751992e5896111b52e9bb5a
Harshal Waghare <88886066+harshalwaghare18@users.noreply.github.com>
2026-01-17 13:26:40 +0530

    Added the files and folders

---
+++ server.js
@@ -0,0 +1,37 @@
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+// const PORT = 4000;
+
+// // Serve static files
+// app.use(express.static(path.join(__dirname, "public")));
+
+// // Serve HTML
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "views", "index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Portfolio running at http://localhost:${PORT}`);
+// });
+
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 4300;
+
+// ✅ Serve static files correctly
+app.use(express.static(path.join(__dirname, "public")));
+
+// ✅ Serve index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Portfolio running at http://localhost:${PORT}`);
+});
